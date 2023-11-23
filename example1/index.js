79a2d958c381571af1e476fbe7afd7bc50d83769
Siddartha <siddartharedddyl@gmail.com>
2023-11-23 12:23:43 +0530

    second push for workflows and also added keys

--- example1/index.js
+++ example1/index.js
@@ -3,7 +3,7 @@ const id = uuidv4();
 function print() {
   const now = new Date();
   const iso = now.toISOString();
-  console.log(`${iso}: ${id}`);  
+  console.log(`${iso}: ${id}`+"   sidd");  
 }
 print(); 
 setInterval(print, 5001);
