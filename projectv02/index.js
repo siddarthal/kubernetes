d2fa61edecf9c77f21a62aeae4182fa76be1efea
siddarthal <63002028+siddarthal@users.noreply.github.com>
2023-11-27 21:43:08 +0530

    Update index.js

--- projectv02/index.js
+++ projectv02/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const host = '0.0.0.0';
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://${host}:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
