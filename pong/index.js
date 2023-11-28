eec4673d26c22234815a94d3346e0336b4ffb7fb
Siddartha <siddartharedddyl@gmail.com>
2023-11-28 12:34:05 +0530

    done final cnages i guess

--- pong/index.js
+++ pong/index.js
@@ -3,11 +3,14 @@ const express = require('express');
 const app = express();
 let counter = 0;
 
-app.get('/', (req, res) => {
+app.get('/pingpong', (req, res) => {
   counter++;
   res.send(`pong ${counter}`);
 });
-
+app.get('/log', (req, res) => {
+  counter++;
+  console.log(`pong ${counter}`);
+});
 app.listen(3000, () => {
   console.log('Listening on port 3000'); 
 })
