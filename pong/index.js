0ec6b8050c1a344d4383fe60b5f52ca1dd28cea7
Siddartha <siddartharedddyl@gmail.com>
2023-11-28 11:30:17 +0530

    pong project

---
+++ pong/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+let counter = 0;
+
+app.get('/pingpong', (req, res) => {
+  counter++;
+  res.send(`pong ${counter}`);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000'); 
+})
