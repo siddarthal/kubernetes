a534b3b9e3949e550386d2468909df58aa0b7617
siddarthal <63002028+siddarthal@users.noreply.github.com>
2023-11-28 22:10:50 +0530

    Update index.js

--- pong/index.js
+++ pong/index.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-
 const app = express();
 const sharedFilePath = 'data/pingpong_count.txt';
-
 let counter = 0;
-
 app.get('/pingpong', (req, res, next) => {
   counter++;
   fs.writeFileSync(sharedFilePath, `${counter}`, { encoding: 'utf8' });
   next();
 });
-// done final changes man
 app.get('/log', (req, res) => {
   fs.readFile(sharedFilePath, 'utf8', (err, data) => {
     if (err) {
@@ -22,13 +18,11 @@ app.get('/log', (req, res) => {
       const timestamp = new Date().toISOString();
       const requestId = uuidv4();
       const logMessage = `${timestamp}: ${requestId}.\nPing / Pongs: ${pingPongCount}`;
-      
       console.log(logMessage);
       res.send(logMessage);
     }
   });
 });
-
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
