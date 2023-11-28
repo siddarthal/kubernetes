14116d5a1111ab776b16a1c5a3d80e9483e6ec13
Siddartha Reddy Legala <sidhureddy.legala@gmail.com>
2023-11-28 22:37:49 +0530

    done with today ra

--- pong/index.js
+++ pong/index.js
@@ -11,4 +11,21 @@ app.get('/pingpong', (req, res, next) => {
   next();
 });
 
+app.get('/log', (req, res) => {
+  fs.readFile(sharedFilePath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading file');
+    } else {
+      const pingPongCount = parseInt(data) || 0;
+      const timestamp = new Date().toISOString();
+      const requestId = uuidv4();
+      const logMessage = `${timestamp}: ${requestId}.\nPing / Pongs: ${pingPongCount}`;
+      console.log(logMessage);
+      res.send(logMessage);
+    }
+  });
+});
 
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
