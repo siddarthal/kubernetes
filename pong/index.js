c84f07de0e369af40dd075498960ecc9013e2ae7
Siddartha Reddy Legala <sidhureddy.legala@gmail.com>
2023-11-28 21:19:05 +0530

    tried adding volumes

--- pong/index.js
+++ pong/index.js
@@ -1,16 +1,34 @@
 const express = require('express');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+const sharedFilePath = '/data/pingpong_count.txt';
+
 let counter = 0;
 
-app.get('/pingpong', (req, res) => {
+app.get('/pingpong', (req, res, next) => {
   counter++;
-  res.send(`pong ${counter}`);
+  fs.writeFileSync(sharedFilePath, `${counter}`, { encoding: 'utf8' });
+  next();
 });
+
 app.get('/log', (req, res) => {
-  counter++;
-  console.log(`pong ${counter}`);
+  fs.readFile(sharedFilePath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading file');
+    } else {
+      const pingPongCount = parseInt(data) || 0;
+      const timestamp = new Date().toISOString();
+      const requestId = uuidv4();
+      const logMessage = `${timestamp}: ${requestId}.\nPing / Pongs: ${pingPongCount}`;
+      
+      console.log(logMessage);
+      res.send(logMessage);
+    }
+  });
 });
+
 app.listen(3000, () => {
-  console.log('Listening on port 3000'); 
-})
+  console.log('Server listening on port 3000');
+});
