87d9cf59191dd8f881f76101f10acf0f594783a3
Siddartha <siddartharedddyl@gmail.com>
2023-12-07 12:04:00 +0530

    last try

--- todo/vite.config.js
+++ todo/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'http://18.117.197.13:8081/', 
   plugins: [react()],
 })
