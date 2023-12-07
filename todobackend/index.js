6ab09df32fc5bedb305dce15d0e8c9c6dbb5f8ae
Siddartha <siddartharedddyl@gmail.com>
2023-12-07 16:21:42 +0530

    push backend

--- todobackend/index.js
+++ todobackend/index.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors({ origin: '*' })); // Enable CORS
+app.use(cors({ origin: "http://52.15.218.118:8081/" })); // Enable CORS
 app.use(bodyParser.json());
 
-let todos = ['red', 'blue', 'green']; // Initial hardcoded todos
-
-app.post('/todos', (req, res) => {
+let todos = ["red", "blue", "green"]; // Initial hardcoded todos
+let count = 0;
+app.post("/todos", (req, res) => {
   const { todo } = req.body;
 
-  if (!Array.isArray(todo) || !todo.every(item => typeof item === 'string')) {
-    return res.status(400).json({ error: 'Invalid todo data' });
+  if (!Array.isArray(todo) || !todo.every((item) => typeof item === "string")) {
+    return res.status(400).json({ error: "Invalid todo data" });
   }
-
+  count++;
+  console.log(`pinged me${count}`);
   todos = todos.concat(todo); // Add new todos to the existing array
-  res.status(201).json({ message: 'Todos added successfully' });
+  res.status(201).json({ message: "Todos added successfully" });
 });
 
-app.get('/todos', (req, res) => {
+app.get("/todos", (req, res) => {
   res.json({ todos });
 });
 
